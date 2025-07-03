d48ed1cb205105e1aa6c07614655ad1c4d386051
nickmol5 <nickpractice55@gmail.com>
2025-07-03 21:12:50 +0100

    Chat funciona

--- clientclass.py
+++ clientclass.py
@@ -1,22 +1,47 @@
 import socket
 
+'''
+USE: Ejecuta primero el codigo del servidor, y en otra terminal ejecuta el codigo del cliente
+'''
+
 class Client:
 	def __init__(self, host, port):
 		self.host = host
 		self.port = port
 		self.socket = None
-		self.state = True
 
 	def connect(self):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.connect((self.host, self.port))
+		print("Connected to the server, if you want to leave press '0'\n")
 
 	def send_message(self): 
 		message = input('You: ')
-		if message == 0:
-			self.socket.close()
-			return
 		self.socket.send(message.encode())
+		if message == '0':
+			self.socket.close()
+			return False
+		return True
 
 	def receive_message(self):
-		data = 
+		data = self.socket.recv(1024).decode()
+		if data == '0':
+			self.socket.close()
+			return False
+		print(f'Server: {data}')
+		return True
+
+
+def main():
+	client = Client('localhost', 9999)
+	client.connect()
+	while True:
+		if not client.send_message():
+			print('\nConnection closed')
+			break
+			
+		if not client.receive_message():
+			print('\nNo message received from the server ')
+			break
+
+main()
--- serverclass.py
+++ serverclass.py
@@ -1,11 +1,15 @@
 import socket
 
+'''
+USE: Ejecuta primero el codigo del servidor, y en otra terminal ejecuta el codigo del cliente
+'''
+
 class Server: 
 	def __init__(self, host, port):
 		self.host = host
 		self.port = port
 		self.sock = None
-		self.clients = {}
+		self.client_socket = None
 
 	def create(self):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,14 +18,39 @@ class Server:
 	def listen(self, num_clientes):
 		self.sock.listen(num_clientes)
 
-	def accept_connection(self, name):
-		client, addr = self.sock.accept()
-		self.clients[name] = {
-			'socket': client,
-			'address': addr
-		}
+	def accept_connection(self):
+		self.client_socket, addr = self.sock.accept()
+		print(f'Server connected to {addr}')
+		print("If you wanna leave, press '0'\n")
 	
-	def 
-	
+	def receive_message(self):
+		message = self.client_socket.recv(1024).decode()
+		if message == '0':
+			self.client_socket.close()
+			return False
+		print(f'Client: {message}')
+		return True
 
 	
+	def send_message(self):
+		message = input('Server: ')
+		self.client_socket.send(message.encode())
+		if message == '0':
+			self.client_socket.close()
+			return False
+		return True
+		
+def main(): 
+	server = Server('localhost', 9999)
+	server.create()
+	server.listen(1)
+	server.accept_connection()
+	while True:
+		if not server.receive_message():
+			print('\nNo message received from the client')
+			break
+		if not server.send_message():
+			print('\nConnection closed')
+			break
+
+main()
